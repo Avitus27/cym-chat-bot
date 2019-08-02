969b28d608d0f2aad5e8982236aaf75ac9bb04c0
Mervyn Galvin <mervyn511@gmail.com>
2019-08-02 16:35:35 +0100

    remove debug, add basic asserts

--- flask-bot/unit_tests/test_basic_root.py
+++ flask-bot/unit_tests/test_basic_root.py
@@ -1,16 +1,14 @@
-#import pdb; pdb.set_trace()
-
 from flask_bot.app import app
 import unittest
+
 
 class TestRoot(unittest.TestCase):
 
     def setUp(self):
-#        pdb.set_trace()
         self.app = app.test_client()
 
     def test_verify_with_no_request_args(self):
         response = self.app.get('/')
 
-        self.assertEqual(1, 1)
-
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'ok')
